6b6642d5a823b3d0509f22f2d10c9acdbd7cb15a
Rivas <rivaldo@gmail.com>
2024-04-09 13:56:49 -0300

    Configurando o model

--- app/Http/Controllers/Main.php
+++ app/Http/Controllers/Main.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Main extends Controller
 {
@@ -13,5 +15,29 @@ class Main extends Controller
             'descricao' => 'Carregar informações!',
         ];
         return view('main', $data);
+    }
+
+    public function users()
+    {
+        //********** get users with raw sql ***************
+        // $users = DB::select('SELECT * FROM users');
+        // dd($users);
+
+        // ************** with query builder ****************
+        // $users = DB::table('users')->get();
+        // dd($users);
+
+        // ************** with query builder toarray ****************
+        // $users = DB::table('users')->get()->toArray();
+        // dd($users);
+
+        // ************** using eloquent ORM ****************
+        $model = new UserModel();
+        $users = $model->all();
+        // dd($users);
+
+        foreach ($users as $user) {
+            echo $user->username . '<br>';
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,4 @@ Route::get('/', function () {
 });
 
 Route::get('/main', [Main::class, 'index']);
+Route::get('/users', [Main::class, 'users']);
