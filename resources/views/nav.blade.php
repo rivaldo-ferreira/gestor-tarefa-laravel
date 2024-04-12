775e4b204ba0fe54156a36363cdfbb5619ef00b2
Rivas <rivaldo@gmail.com>
2024-04-12 14:57:13 -0300

    login e logout

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -5,7 +5,9 @@
                 <h3 class="text-primary">Gestor de Tarefas</h3>
             </div>
             <div class="col p-3 text-end">
-                [username] | [logout]
+                <span><i class="bi bi-person me-2"></i> {{ session()->get('username') }}</span>
+                <span class="mx-3 opacity-50"><i class="bi bi-grip-vertical"></i></span>
+                <a href="{{ route('logout') }}" class="btn btn-outline-danger"><i class="bi bi-box-arrow-right me-2"></i>Sair</a>
             </div>
         </div>
     </div>
--- resources/views/template/main_layout.blade.php
+++ resources/views/template/main_layout.blade.php
@@ -5,7 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
+    {{-- BOOTSTRAP --}}
     <link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css') }}">
+    {{-- BOOTSTRAP ICONS --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 
 <body>
